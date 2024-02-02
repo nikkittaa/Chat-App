291a365f00fc4a71aabb7f30d9f5a080cdb899f3
Nikita <nikitanb04@gmail.com>
2024-02-02 12:56:00 +0530

    added avatars to user

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ const jwt = require('jsonwebtoken');
 var cookieParser = require('cookie-parser');
 var cors = require('cors');
 var bcrypt = require('bcryptjs');
+const ws = require('ws');
 const app = express();
 
 app.use(express.json());
@@ -84,4 +85,29 @@ app.post('/register' , async (req, res) => {
 
 });
 
-app.listen(4040);
+const server = app.listen(4040);
+
+const wss =  new ws.WebSocketServer({server});
+wss.on('connection', (connection, req) => {
+    const cookies = req.headers.cookie;
+    if(cookies){
+        const tokenCookieString = cookies.split(';').find(str => str.startsWith('token='));
+        if(tokenCookieString){
+            const token = tokenCookieString.split('=')[1];
+            if(token){
+                jwt.verify(token, jwtSecret, {}, (err, userData) => {
+                    if(err) throw err;
+                    const {userId, username} = userData;
+                    connection.userId = userId;
+                    connection.username = username;
+                });
+            }
+        }
+    }
+    [...wss.clients].forEach(client => {
+        client.send(JSON.stringify(
+            {online: [...wss.clients].map(c => ({userId: c.userId, username: c.username}))
+        }
+        ));
+    });
+});
--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -1,13 +1,14 @@
 import React, { useContext } from 'react'
 import RegisterAndLoginForm from './RegisterAndLoginForm'
 import { UserContext } from './UserContext'
+import Chat from './Chat';
 
 const Routes = () => {
 
     const {username, id} = useContext(UserContext);
 
     if(username){
-        return "logged in! "+ username;
+        return <Chat/>;
     }
     return (
         <RegisterAndLoginForm/>
